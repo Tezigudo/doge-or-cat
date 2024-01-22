5fbb99b8b37434670900d92ab53f5a0b67f24a0e
Preawpan Thamapipol <80502090+Tezigudo@users.noreply.github.com>
2024-01-22 09:48:34 +0700

    add train file

---
+++ app.py
@@ -0,0 +1,20 @@
+import gradio as gr
+from fastai.vision.all import *
+
+
+# Define a function for model inference
+def classify_image(inp):
+    learn = load_learner('model.pkl')
+    img = PILImage.create(inp)
+    img.thumbnail((128,128))
+    pred,pred_idx,probs = learn.predict(img)
+    if pred == "dog": 
+        result = f"The image is a Dog with probability {probs[pred_idx]:.4f}"
+    else:
+        result = f"The image is look like a Cat {probs[pred_idx]:.4f}"
+    return result
+
+example = ['img/cat.webp', 'img/dog.jpeg']
+
+iface = gr.Interface(fn=classify_image, inputs="image", outputs="text", examples=example)
+iface.launch()
